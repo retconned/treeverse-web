94705a9009d3dbc58e71e5ded3a9e2398fe7005a
retconned <retcnned@gmail.com>
2022-07-02 18:20:35 +0200

    Added Google fonts Poppins & Barlow

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+// const defaultTheme = require("tailwindcss/defaultTheme")
 module.exports = {
-   content: [
+  content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        barlow: ["Barlow", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
