1ce2a157b73c33789811d941363bd14ca9677641
retconned <retcnned@gmail.com>
2022-07-04 13:06:30 +0200

    Followup section mobile responsive design update

--- components/FollowupSection.tsx
+++ components/FollowupSection.tsx
@@ -1,8 +1,5 @@
 import type { NextPage } from "next";
 import Image from "next/image";
-import Link from "next/link";
-
-// nav button component
 
 import CharDesign from "../public/Character-design.webp";
 import IslandDesign from "../public/Environment_floating-island-expansion.webp";
@@ -10,9 +7,9 @@ import LazerIslandDesign from "../public/Laze island.webp";
 
 const FollowupSection: NextPage = () => {
   return (
-    <main className="flex h-fit w-full flex-col bg-treeGray py-14 md:flex-row">
-      <div className="space-y-6 p-10 font-poppins md:w-[50vw] md:space-y-10 md:p-32">
-        <p className="text-3xl font-semibold text-treeGreen md:w-2/3 md:text-7xl">
+    <main className="flex w-full flex-col bg-treeGray py-0 sm:flex-col md:h-[91vh] md:flex-row">
+      <div className="space-y-6 p-10 font-poppins md:space-y-10 md:p-32 lg:w-[80vw] ">
+        <p className=" text-3xl font-semibold text-treeGreen md:w-2/3 md:text-7xl">
           What is Treeverse?
         </p>
         <p className="font-semibold text-black ">
@@ -27,35 +24,34 @@ const FollowupSection: NextPage = () => {
           primal energy and slay bosses to test your skills.
         </p>
       </div>
-      <div className="relative flex h-[100vh] w-screen flex-col items-center justify-center bg-treeGray  px-0 py-0 md:h-screen md:w-[50vw] md:flex-row md:px-8 md:py-0">
-        <div className="absolute right-5 bg-blue-500 md:right-auto md:top-0">
-          <div className="absolute top-[-290px] right-[170px] h-[calc(353px/1.5)] w-[calc(202px/1.5)] overflow-hidden rounded-2xl border-4 border-blue-400 md:top-[60px] md:right-[100px] md:h-[353px] md:w-[202px]">
-            <Image
-              src={CharDesign}
-              layout="fill"
-              objectFit="cover"
-              draggable={false}
-              alt="Character design"
-            />
-          </div>
-          <div className="absolute top-[-25px] right-[-5px] h-[calc(501px/1.5)] w-[calc(348px/1.5)] overflow-hidden rounded-2xl border-4 border-blue-400 md:top-[150px] md:right-[-350px]  md:h-[501px] md:w-[348px]">
-            <Image
-              src={IslandDesign}
-              layout="fill"
-              objectFit="cover"
-              draggable={false}
-              alt="Floating island design"
-            />
-          </div>
-          <div className="absolute top-[280px] right-[20px] h-[calc(280px/1.5)] w-[calc(480px/1.5)] overflow-hidden rounded-2xl border-4 border-blue-400 md:top-[580px] md:right-[-40px] md:h-[280px] md:w-[480px]">
-            <Image
-              src={LazerIslandDesign}
-              layout="fill"
-              objectFit="cover"
-              draggable={false}
-              alt="Enviorment design with laser in the middle"
-            />
-          </div>
+
+      <div className="relative flex h-[calc(100vh-25vh)] w-full flex-col justify-between md:hidden md:h-[91vh] md:min-w-[30vw] lg:flex ">
+        <div className="absolute top-[6%] left-[70%] h-fit w-[101px] overflow-hidden rounded-xl border-4  border-white md:left-[44%] md:w-[193px] lg:left-[60%]">
+          <Image
+            unoptimized={true}
+            src={CharDesign}
+            alt="Character design"
+            layout="responsive"
+            objectFit="fill"
+          />
+        </div>
+        <div className="absolute top-[8%] left-[5%] h-fit w-[183px] overflow-hidden rounded-xl border-4  border-white md:left-[25%] md:top-[35%] md:w-[166px] lg:top-[25%] lg:left-[25%] lg:w-[266px]">
+          <Image
+            unoptimized={true}
+            src={IslandDesign}
+            alt="Floating island environment design"
+            layout="responsive"
+            objectFit="fill"
+          />
+        </div>
+        <div className="absolute top-[50%] left-[5%] h-fit w-[350px] overflow-hidden rounded-xl border-4 border-white md:top-[62%] md:w-[250px] lg:left-[11%] lg:w-[250px] 2xl:left-[40%]">
+          <Image
+            unoptimized={true}
+            src={LazerIslandDesign}
+            alt="island environment design with laser in the middle"
+            layout="responsive"
+            objectFit="fill"
+          />
         </div>
       </div>
     </main>
